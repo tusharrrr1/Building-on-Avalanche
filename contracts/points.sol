8b9923bc7e376d9584d91b504d2a7a9cc524b635
tusharrrr1 <161235330+tusharrrr1@users.noreply.github.com>
2024-08-02 01:37:12 +0530

    Update points.sol

--- contracts/points.sol
+++ contracts/points.sol
@@ -1,20 +1,92 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.20;
+pragma solidity 0.8.19;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+interface IERC20 {
+    function totalSupply() external view returns (uint);
+    function balanceOf(address account) external view returns (uint);
+    function transfer(address recipient, uint amount) external returns (bool);
+    
+    event Transfer(address indexed from, address indexed to, uint amount);
+}
 
-contract DegenToken is ERC20, ERC20Burnable, Ownable, ERC20Permit {
-    constructor(address initialOwner)
-        ERC20("Degen Token", "MTK")
-        Ownable(initialOwner)
-        ERC20Permit("Degen Token")
-    {}
+contract ERC20 is IERC20 {
+    address public immutable owner;
+    uint public totalSupply;
+    mapping (address => uint) public balanceOf;
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    struct Item {
+        uint itemId;
+        string itemName;
+        uint itemPrice;
+    }
+    
+    mapping(uint => Item) public items;
+    uint public itemCount;
+
+    constructor() {
+        owner = msg.sender;
+        totalSupply = 0;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only the contract owner can execute this function");
+        _;
+    }
+
+    string public constant name = "Degen";
+    string public constant symbol = "DGN";
+    uint8 public constant decimals = 0;
+
+    function transfer(address recipient, uint amount) external returns (bool) {
+        require(balanceOf[msg.sender] >= amount, "The balance is insufficient");
+
+        balanceOf[msg.sender] -= amount;
+        balanceOf[recipient] += amount;
+
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function mint(address receiver,uint amount) external onlyOwner {
+        balanceOf[receiver] += amount;
+        totalSupply += amount;
+        emit Transfer(address(0), receiver, amount);
+    }
+
+    function burn(uint amount) external {
+        require(amount > 0, "Amount should not be zero");
+        require(balanceOf[msg.sender] >= amount, "The balance is insufficient");
+        balanceOf[msg.sender] -= amount;
+        totalSupply -= amount;
+
+        emit Transfer(msg.sender, address(0), amount);
+    }
+    
+    function addItem(string memory itemName, uint256 itemPrice) external onlyOwner {
+        itemCount++;
+        Item memory newItem = Item(itemCount, itemName, itemPrice);
+        items[itemCount] = newItem;
+    }
+
+    function getItems() external view returns (Item[] memory) {
+        Item[] memory allItems = new Item[](itemCount);
+        
+        for (uint i = 1; i <= itemCount; i++) {
+            allItems[i - 1] = items[i];
+        }
+        
+        return allItems;
+    }
+    
+    function redeem(uint itemId) external {
+        require(itemId > 0 && itemId <= itemCount, "Invalid item ID");
+        Item memory redeemedItem = items[itemId];
+        
+        require(balanceOf[msg.sender] >= redeemedItem.itemPrice, "Insufficient balance to redeem");
+        
+        balanceOf[msg.sender] -= redeemedItem.itemPrice;
+        balanceOf[owner] += redeemedItem.itemPrice;
+        emit Transfer(msg.sender, address(0), redeemedItem.itemPrice);
+        
     }
 }
